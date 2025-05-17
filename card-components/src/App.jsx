b118f23440485960e47e7efcaf12000e2c3e1086
ParasNn <40841618+ParasNn@users.noreply.github.com>
2025-05-17 12:57:23 -0500

    Props for Cards
    Added new Oreo card using the card component's properties

--- card-components/src/App.jsx
+++ card-components/src/App.jsx
@@ -1,10 +1,13 @@
+import parasPic from './assets/banner.png'
+import oreoPic from './assets/oreo.jpeg'
 import Card from './Card.jsx'
 
 function App() {
 
     return (
         <>
-            <Card />
+            <Card name="Paras Nijhawan" profilePic={parasPic} text="I'm learning react" />
+            <Card name="Oreo" profilePic={oreoPic} text="I'm a dog" />
         </>
 
     );
--- card-components/src/Card.jsx
+++ card-components/src/Card.jsx
@@ -1,11 +1,10 @@
-import profilePic from './assets/banner.png'
 
-function Card() {
+function Card(props) {
     return (
         <div className="card">
-            <img className="card-image" src={profilePic} alt="profile picture"></img>
-            <h2 className="card-title">Paras Nijhawan</h2>
-            <p className="card-text">I'm learning react</p>
+            <img className="card-image" src={props.profilePic} alt="profile picture"></img>
+            <h2 className="card-title">{props.name}</h2>
+            <p className="card-text">{props.text}</p>
         </div>
     );
 
